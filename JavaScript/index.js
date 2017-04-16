12499afc6408ad01244852f4298220b204f077cf
dudu <1161071902@qq.com>
2017-04-16 20:49:55 +0800

    4.16

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,6 +1,7 @@
 window.addEventListener("load", function() {
 	//导航
 	var indexNav = 0;
+	var indexHistory = 0;
 	$('.djsNav li').mouseover(function() {
 		indexNav = $(this).index()
 		$('.djsBorder').eq(indexNav).css('width', '100%')
@@ -11,4 +12,16 @@ window.addEventListener("load", function() {
 	})
 	
 	$('.djs-banner').css('left','28%').css('transform','rotate(0deg)').css('opacity','1')
+	
+	
+	
+	//公司历程
+	$('.history-content>ul>li').mouseover(function() {
+		indexHistory = $(this).index()
+		$('.history-content>ul>li').eq(indexHistory).children('div').css('display', 'block')
+	})
+	$('.history-content>ul>li').mouseout(function() {
+		indexHistory = $(this).index()
+		$('.history-content>ul>li').eq(indexHistory).children('div').css('display', 'none')
+	})
 })
